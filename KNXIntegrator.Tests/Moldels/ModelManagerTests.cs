2a0e64047bcc819f663b5db46efebd6a5f5fd4c6
Daichi9764 <145645508+Daichi9764@users.noreply.github.com>
2024-08-22 12:28:38 +0200

    WIP

--- KNXIntegrator.Tests/Moldels/ModelManagerTests.cs
+++ KNXIntegrator.Tests/Moldels/ModelManagerTests.cs
@@ -8,6 +8,28 @@ public class ModelManagerTests{
     private FunctionalModelDictionary funDict = new FunctionalModelDictionary();
 
     public ModelManagerTests(){
+        //simulate creation of models by user
+        var model1 = new FunctionalModel("key1",1);
+        var model2 = new FunctionalModel("key2",2);
+        funDict.Add_FunctionalModel(model1);
+        funDict.Add_FunctionalModel(model2);
+
+        //simulate the mapping between models and addresses
+        keysDB.Add("key1","LightLivingRoom");
+        keysDB.Add("key2","LightBedroom");
+
+        //given a Cmd "LightLivingRoom", what IE should be returned ?
+        var myGrpAddr = addrRepo.GetGrpAddr("LightLivingRoom");
+        var myModelKey = keysDB.GetByKey2("LightLivingRoom");
+        var myModel = funDict.Get_FunctionalModel(myModelKey);
+        var commands = Transfer.GetSendCommands(myGrpAddr,myModel);
+        var expected = Transfer.GetExpectedCommands(myGrpAddr,myModel);
+        KNXManager.WriteFrames(commands);
+        var actual = await KNXManager.ReadFrames(source_addr : myGrpAddr.addrHexa);
+        foreach(frame in expected){
+            Assert.Equals(frame,actual.Get(frame.addr));
+        }
+
 
 
     }
---
+++ KNXIntegrator/Models/Interface/IGroupCommunication.cs
@@ -0,0 +1,12 @@
+namespace KNXIntegrator.Models;
+
+public interface IGroupCommunication
+{
+    Task WriteAsync(Frame frame);
+
+    Task WriteAsync(List<Frame> frames);
+
+    Task<Frame> ReadAsync(int srcAddr);
+
+    Task<List<Frame>> ReadAsync(List<int> srcAddrs);
+}
