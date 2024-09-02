2624d2993063c0dfc43027ed958eeda6319fd497
Daichi9764 <145645508+Daichi9764@users.noreply.github.com>
2024-09-02 14:53:29 +0200

    Basic operation OK, test OK, waiting merge

--- KNXIntegrator/Models/Implementation/ModelManager.cs
+++ KNXIntegrator/Models/Implementation/ModelManager.cs
@@ -3,8 +3,9 @@
 namespace KNXIntegrator.Models;
 
 public class ModelManager:IModelManager{
-    // private IKeyPairDatabase keysDB = new KeyPairDatabase();
-    // private IGrpAddrRepository addrRepo = new GrpAddrRepository();
-    // private FunctionalModelDictionary funDict = new FunctionalModelDictionary();
+    public string RunAnalysis()
+    {
+        return ("la méthode RunAnalysis n'est pas encore implémentée.");
+    }
 
 }
--- KNXIntegrator/Models/Interface/IModelManager.cs
+++ KNXIntegrator/Models/Interface/IModelManager.cs
@@ -1,5 +1,7 @@
 namespace KNXIntegrator.Models;
 
-public interface IModelManager{
-    
+public interface IModelManager
+{
+    public string RunAnalysis();
+
 }
